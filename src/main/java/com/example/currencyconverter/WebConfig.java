535228f3b1e8d1bd27a295bc685ad57520a55b22
Himanshu Wani <hwanife2022@bharatedu.co.in>
2025-08-28 22:39:25 +0530

    Update WebConfig.java

--- src/main/java/com/example/currencyconverter/WebConfig.java
+++ src/main/java/com/example/currencyconverter/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://currency-converter-zandacross.netlify.app/")   // front end origin
+                        .allowedOrigins("https://currency-converter-zandacross.netlify.app")   // front end origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
             }
         };
